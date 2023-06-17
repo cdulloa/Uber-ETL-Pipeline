2710e4ebeb0f1a6e4d834540bdd1808d686d2f7f
cdulloa <corbinulloa619@gmail.com>
2023-06-16 21:52:04 -0700

    Added documetation

--- mage_files/extract.py
+++ mage_files/extract.py
@@ -1,8 +1,12 @@
 import io
 import pandas as pd
 import requests
+
+# Check if 'data_loader' is not defined in global namespace and import it if necessary
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
+
+# Check if 'test' is not defined in global namespace and import it if necessary
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
@@ -10,7 +14,13 @@ if 'test' not in globals():
 @data_loader
 def load_data_from_api(*args, **kwargs):
     """
-    Template for loading data from API
+    Template for loading data from an API.
+
+    This function sends a GET request to a specified URL and retrieves CSV data.
+    The data is then converted into a Pandas DataFrame and returned.
+
+    Returns:
+        pd.DataFrame: The loaded data as a Pandas DataFrame.
     """
     url = 'https://storage.googleapis.com/uber-data-engineering-proj-etl/uber_data.csv'
     response = requests.get(url)
@@ -21,6 +31,15 @@ def load_data_from_api(*args, **kwargs):
 @test
 def test_output(output, *args) -> None:
     """
-    Template code for testing the output of the block.
+    Template code for testing the output of a block.
+
+    This function checks if the output is not None and raises an assertion error if it is.
+
+    Args:
+        output: The output to be tested.
+        *args: Additional arguments (not used in this function).
+
+    Raises:
+        AssertionError: If the output is None.
     """
     assert output is not None, 'The output is undefined'
--- mage_files/load.py
+++ mage_files/load.py
@@ -12,17 +12,42 @@ if 'data_exporter' not in globals():
 def export_data_to_big_query(data, **kwargs) -> None:
     """
     Template for exporting data to a BigQuery warehouse.
-    Specify your configuration settings in 'io_config.yaml'.
 
-    Docs: https://docs.mage.ai/design/data-loading#bigquery
+    This function exports data to a BigQuery warehouse using the Mage AI framework.
+    It relies on the 'mage_ai.io.bigquery.BigQuery' class for the export operation.
+
+    Args:
+        data (dict): A dictionary containing data frames to be exported. The keys represent table names,
+                     and the values are pandas DataFrame objects containing the corresponding data.
+        **kwargs: Additional keyword arguments (not used in this function).
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    Example:
+        data = {
+            'table1': DataFrame({'col1': [1, 2, 3], 'col2': ['A', 'B', 'C']}),
+            'table2': DataFrame({'col3': [4, 5, 6], 'col4': ['D', 'E', 'F']})
+        }
+        export_data_to_big_query(data)
     """
+    # Path to the 'io_config.yaml' file
     config_path = path.join(get_repo_path(), 'io_config.yaml')
+
+    # Configuration profile to use
     config_profile = 'default'
 
+    # Export each data frame to BigQuery
     for key, value in data.items():
+        # Construct the table_id using the key
         table_id = 'fabled-essence-389307.uber_data_engineering_yt.{}'.format(key)
+
+        # Export the data frame to BigQuery
         BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
             DataFrame(value),
             table_id,
-            if_exists='replace',  # Specify resolution policy if table name already exists
+            if_exists='replace'  # Specify resolution policy if table name already exists
         )
